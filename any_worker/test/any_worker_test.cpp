8ede73f92761f42cb8e8726c25ae9964f84c4fb1
Philipp Leemann <pleemann@anybotics.com>
2024-02-19 13:49:44 +0000

    Merge branch 'fix/any_worker/timing_tests' into 'master'
    [any_worker] added rtprio to rate test to ensure it passes under heavy system load
    
    Closes #UNKNOWN
    
    GitOrigin-RevId: b84e8a24639537a0d9ec76bd55e39d966de96c9d

--- any_worker/test/any_worker_test.cpp
+++ any_worker/test/any_worker_test.cpp
@@ -1,8 +1,21 @@
 // gtest
 #include <gtest/gtest.h>
 
+// std
+#include <sched.h>   // For sched_setscheduler
+#include <unistd.h>  // For getpid()
+
+#include <message_logger/message_logger.hpp>
+
 // Run all the tests that were declared with TEST()
 int main(int argc, char** argv) {
+  // Increase the priority of this process, as some of the tests are timing critical.
+  sched_param params{};
+  params.sched_priority = 20;
+  if (sched_setscheduler(getpid(), SCHED_FIFO, &params) != 0) {
+    MELO_WARN("Failed to set rtprio for the test - execution can be unreliable!");
+  }
+
   testing::InitGoogleTest(&argc, argv);
   return RUN_ALL_TESTS();
 }
